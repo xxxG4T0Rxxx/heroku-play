3209ce9da94eb38c14ed1b57798e04bd2db0df7a
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-09-23 03:19:10 -0400

    testing query concat

--- api_dynamic.php
+++ api_dynamic.php
@@ -9,7 +9,7 @@
 
     //$test = json_decode($_POST,true);
     //$sword = $test["word"];
-    $rackback = $_POST.word;
+    $rackback = "EPPR";
 
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
