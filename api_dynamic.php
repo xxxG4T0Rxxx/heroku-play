4897ce2d2223fe096fb65c67b2490008e2c040af
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-09-23 01:06:36 -0400

    json debugging

--- api_dynamic.php
+++ api_dynamic.php
@@ -8,12 +8,13 @@
     if (!$dbhandle) die ($error);
 
     $test = json_decode($_POST);
+    $word = $test["word"];
 
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination",
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
+    $query = "SELECT rack, words FROM racks WHERE rack=$word";
 
     //this next line could actually be used to provide user_given input to the query to
     //avoid SQL injection attacks
