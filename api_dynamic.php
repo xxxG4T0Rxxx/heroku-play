7408a162e78c54ccd07e183b960c978aa878808f
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-09-23 03:15:08 -0400

    debugging attempt

--- api_dynamic.php
+++ api_dynamic.php
@@ -54,5 +54,5 @@
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode($results);
+    echo ($rackback);
 ?>
