68bc0a74ad81669bfe61efde954384749c2196ff
xxxG4T0Rxxx <nickmcd33@gmail.com>
2019-09-23 20:53:29 -0400

    trying to retrieve racks and words

--- api_dynamic.php
+++ api_dynamic.php
@@ -55,5 +55,5 @@
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode($testRack);
+    echo json_encode($results);
 ?>
